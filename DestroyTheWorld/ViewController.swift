307db861bc80130ee535f1de264d788737fa0a5e
ICF2025846 <icf2025846@openvpn-client.awsvpn.laquinta.com>
2017-05-17 12:46:22 +0530

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by ICF2025846 on 17/05/17.
+//  Copyright © 2017 ICF2025846. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
